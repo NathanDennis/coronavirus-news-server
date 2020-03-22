d0c5d3c06440f8777646a555c7fc5dd34f407027
NathanDennis <nathdennis@googlemail.com>
2020-03-22 03:56:09 -0400

    init commit

---
+++ server/app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+
+const dotenv = require('dotenv')
+dotenv.config()
+const { APIKEY, PORT } = process.env
+
+const cron = require('node-cron')
+const fetch = require('node-fetch')
+
+const today = new Date().toJSON().slice(0,10)
+const url = `https://newsapi.org/v2/everything?q=coronavirus&from=${today}&sortBy=popularity&apiKey=${APIKEY}`
+
+
+let articleData = ''
+
+const getAPIData = async (apiURL) => {
+    const response = await fetch(apiURL, {
+        method: 'GET',
+    })
+
+    return await response.json()
+}
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+getAPIData(url).then((data) => {
+    articleData.length = 0
+    articleData = data.articles
+}).catch(error => console.log(error))
+
+cron.schedule('* 0 * * * *', () => {
+    getAPIData().then((data) => {
+        articleData.length = 0
+        articleData = data
+    })
+})
+
+app.get('/', (req, res)=> {
+    res.send(articleData)
+})
+
+app.listen(PORT, () => {
+    console.log('Article content server running')
+})
